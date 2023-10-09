275386da69ebba9bb6877b0e048a9a032acf88bf
aghafourian <68355687+aghafourian@users.noreply.github.com>
2023-10-09 03:47:29 -0700

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python testing.py -n 12_3_3_swav_best -d 2 -fast 0 -p /path/to/model
-python testing.py -n 12_3_3_swav_best -d 2 -fast 1
-python testing.py -n 12_3_3_swav_skip_cov_best -d 2 -fast 1
-python testing.py -n 12_3_3_resnet50_best -d 2 -fast 1
-python testing.py -n 12_3_3_resnet50_skip_cov_best -d 2 -fast 1
-python testing.py -n 12_3_3_scratch_best -d 2 -fast 1
-
